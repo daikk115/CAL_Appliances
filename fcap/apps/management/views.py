808f649aadfe0c8a0dcd1fa735d6a19fe5e431ce
daikk115 <daikk115@gmail.com>
2016-11-27 01:52:03 +0700

    Update UI: add main page

---
+++ fcap/apps/management/views.py
@@ -0,0 +1,118 @@
+from django.views.generic import TemplateView
+
+
+class AppView(TemplateView):
+    template_name = 'management/index.html'
+    page = "management/btn_and_popup_{}.html".format('app')
+
+    def get(self, request, *args, **kwargs):
+        table = {
+            'category': ['Name', 'Flavor', 'IP Address', 'Actions'],
+            'rows': [
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+    def post(self, request, *args, **kwargs):
+        table = {
+            'category': ['App Name', 'Flavor', 'IP Address', 'Actions'],
+            'rows': [
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4'],
+                ['1', '2', '3', '4']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+
+class NetworkView(TemplateView):
+    template_name = 'management/index.html'
+    page = "management/btn_and_popup_{}.html".format('network')
+
+    def get(self, request, *args, **kwargs):
+        table = {
+            'category': ['Subnet ID', 'Name', 'CIDR', 'Provider', 'Security_group', 'Actions'],
+            'rows': [
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+    def post(self, request, *args, **kwargs):
+        table = {
+            'category': ['Subnet ID', 'Name', 'CIDR', 'Provider', 'Security_group', 'Actions'],
+            'rows': [
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6'],
+                ['1', '2', '3', '4', '5', '6']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+
+class ProviderView(TemplateView):
+    template_name = 'management/index.html'
+    page = "management/btn_and_popup_{}.html".format('provider')
+
+    def get(self, request, *args, **kwargs):
+        table = {
+            'category': ['Name', 'Cloud Config', 'Actions'],
+            'rows': [
+                ['1', '2', '3'],
+                ['1', '2', '3'],
+                ['1', '2', '3']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+    def post(self, request, *args, **kwargs):
+        table = {
+            'category': ['Name', 'Cloud Config', 'Actions'],
+            'rows': [
+                ['1', '2', '3'],
+                ['1', '2', '3'],
+                ['1', '2', '3']
+            ]
+        }
+        return self.render_to_response({
+            'table': table,
+            'page': self.page
+        })
+
+
+class AboutView(TemplateView):
+    template_name = 'management/about.html'
+
+    def get(self, request, *args, **kwargs):
+        table = {
+            'category': ['App Name', 'Flavor', 'IP Address', 'Actions']
+        }
+        return self.render_to_response({})
